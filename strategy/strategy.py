02a7eb73fd12a23a9592a80ec10541242335aa53
Ethan Pedersen <ethanp55@gmail.com>
2022-06-07 15:24:22 -0600

    Override string method in strategy results class for neater output

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -3,6 +3,7 @@ from market_proxy.currency_pairs import CurrencyPairs
 from market_proxy.trades import Trade
 from typing import Optional
 
+
 class Strategy:
     def __init__(self, description: str, starting_idx: int) -> None:
         self.description = description
--- strategy/strategy_results.py
+++ strategy/strategy_results.py
@@ -13,3 +13,9 @@ class StrategyResults:
     n_losses: int
     longest_win_streak: int
     longest_loss_streak: int
+
+    def __str__(self):
+        return f'RESULTS:\nreward = {self.reward}\nday fees = {self.day_fees}\nnet reward = {self.net_reward}' \
+               f'\navg pips risked = {self.avg_pips_risked}\nbuys = {self.n_buys}\nsells = {self.n_sells}' \
+               f'\nwins = {self.n_wins}\nlosses = {self.n_losses}\nlongest win streak = {self.longest_win_streak}' \
+               f'\nlongest loss streak = {self.longest_loss_streak}'
